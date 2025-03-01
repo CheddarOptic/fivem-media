6dadd2869f5183c6af3d3fbf5c9634326c7ecedd
Chris <christophbeuter@gmail.com>
2025-03-01 04:57:30 +0100

    changed to reverse proxy

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ApiKeyAuthGuard } from './guards/api-key.guard';
-import * as fs from 'fs';
 
 async function bootstrap() {
 
-  const privkey = process.env.PRIVKEY;
-  const fullchain = process.env.FULLCHAIN;
-
-  const httpsOptions = {
-    key: fs.readFileSync(`${privkey}`),
-    cert: fs.readFileSync(`${fullchain}`),
-  };
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
 
